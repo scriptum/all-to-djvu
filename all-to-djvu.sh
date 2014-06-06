ee013523ce20056d36c6552f398dc117c1cbad00
Pavel Roschin <roshin@scriptumplus.ru>
2014-06-06 11:26:25 +0400

    Fix indents

--- all-to-djvu.sh
+++ all-to-djvu.sh
@@ -64,111 +64,118 @@ N=1
 while [ $# -gt 0 ]
 do
 	case "$1" in
--c|--cover)
-	if [ ! -f "$2" ]
-	then
-		echo "Cannot find file: $2" > /dev/stderr
-	else
-		convert "$2" .cover.ppm
-		c44 -dpi $(get_dpi "$2") .cover.ppm .cover.djvu
-		COVER=".cover.djvu"
-	fi
-	shift
-;;
--p|--poster)
-	if [ ! -f "$2" ]
-	then
-		echo "Cannot find file: $2" > /dev/stderr
-	else
-		convert -unsharp 0x3+2 -posterize 2 "$2" .cover.ppm
-		cpaldjvu -dpi $(get_dpi "$2") .cover.ppm .cover.djvu
-		COVER=".cover.djvu"
-	fi
-	shift
-;;
--b|--blur)
-	BLUR="-blur $2"
-	shift
-;;
--g|--gamma)
-	GAMMA="-gamma $2"
-	shift
-;;
--l|--level)
-	LEVEL="-level $2"
-	shift
-;;
--k|--keep)
-	CLEAN="0"
-;;
--o|--out)
-	OUT="$2"
-	shift
-;;
--s|--sharp)
-	SHARP="$2"
-	shift
-;;
--u|--upscale)
-	UP="-resize $2"
-	shift
-;;
--d|--downscale)
-	DOWN="-resize $2"
-	shift
-;;
--r|--rotate)
-	ROT="-rotate $2"
-	shift
-;;
--f|--filter)
-	case "$2" in
-	median)
-		FILTER="-median 0x1"
+	-c|--cover)
+		if [ ! -f "$2" ]
+		then
+			echo "Cannot find file: $2" > /dev/stderr
+		else
+			convert "$2" .cover.ppm
+			c44 -dpi $(get_dpi "$2") .cover.ppm .cover.djvu
+			COVER=".cover.djvu"
+		fi
+		shift
 	;;
-	paint)
-		FILTER="-paint 1"
+	-p|--poster)
+		if [ ! -f "$2" ]
+		then
+			echo "Cannot find file: $2" > /dev/stderr
+		else
+			convert -unsharp 0x3+2 -posterize 2 "$2" .cover.ppm
+			cpaldjvu -dpi $(get_dpi "$2") .cover.ppm .cover.djvu
+			COVER=".cover.djvu"
+		fi
+		shift
+	;;
+	-b|--blur)
+		BLUR="-blur $2"
+		shift
+	;;
+	-P|--PDF)
+		PDF="1"
+	;;
+	-g|--gamma)
+		GAMMA="-gamma $2"
+		shift
+	;;
+	-l|--level)
+		LEVEL="-level $2"
+		shift
+	;;
+	-k|--keep)
+		CLEAN="0"
+	;;
+	-o|--out)
+		OUT="$2"
+		shift
+	;;
+	-s|--sharp)
+		SHARP="$2"
+		shift
+	;;
+	-u|--upscale)
+		UP="-resize $2"
+		shift
+	;;
+	-d|--downscale)
+		DOWN="-resize $2"
+		shift
+	;;
+	-r|--rotate)
+		ROT="-rotate $2"
+		shift
+	;;
+	-f|--filter)
+		case "$2" in
+		median)
+			FILTER="-median 0x1"
+		;;
+		paint)
+			FILTER="-paint 1"
+		;;
+		*)
+			FILTER=""
+		;;
+		esac
+		shift
+	;;
+	-*|--help)
+		help
 	;;
 	*)
-		FILTER=""
+		UNROTATE=""
+		UNROTATE="-deskew 100%"
+
+		ENHANCE=" $BLUR $GAMMA -unsharp 0x10+$SHARP+0"
+		ENHANCE=" $BLUR $GAMMA -unsharp 0x5+$SHARP+0 ( +clone -blur 0x20 ) -compose Divide_Src -composite -normalize"
+
+		#THRESHOLD=""
+		#THRESHOLD="-monochrome"
+		THRESHOLD="-threshold 60%"
+
+		#FILTER="-statistic Mode 2x2"
+		#FILTER="-negate -median 2x2 -negate"
+		#FILTER=""
+		#FILTER="-paint 1"
+
+
+		OPTS="$ROT $UNROTATE $LEVEL $AUTOLEVEL $UP $ENHANCE $DOWN $THRESHOLD $FILTER"
+		if [ ! -f "$1" ]
+		then
+			echo "Cannot find file: $1" > /dev/stderr
+		else
+			NAME=$(printf "%06d" $N)
+			convert "$1" $OPTS .$NAME.pbm
+			#echo PSNR: $(compare -metric PSNR $1 .tmp.pbm /dev/null 2>&1)
+			cjb2 -dpi $(get_dpi ".$NAME.pbm") -lossy -losslevel $LOSSLVL .$NAME.pbm .$NAME.djvu
+			
+			echo "$1" $(wc -c < .$NAME.djvu) "bytes"
+			((N++))
+		fi
 	;;
-	esac
-	shift
-;;
-*)
-UNROTATE=""
-UNROTATE="-deskew 100%"
-
-ENHANCE=" $BLUR $GAMMA -unsharp 0x10+$SHARP+0"
-ENHANCE=" $BLUR $GAMMA -unsharp 0x5+$SHARP+0 ( +clone -blur 0x20 ) -compose Divide_Src -composite -normalize"
-
-THRESHOLD=""
-THRESHOLD="-monochrome"
-THRESHOLD="-threshold 60%"
-
-#FILTER="-statistic Mode 2x2"
-#FILTER="-negate -median 2x2 -negate"
-#FILTER=""
-#FILTER="-paint 1"
-
-
-OPTS="$ROT $UNROTATE $LEVEL $AUTOLEVEL $UP $ENHANCE $DOWN $THRESHOLD $FILTER"
-	if [ ! -f "$1" ]
-	then
-		echo "Cannot find file: $1" > /dev/stderr
-	else
-		NAME=$(printf "%06d" $N)
-		convert "$1" $OPTS .$NAME.pbm
-		#echo PSNR: $(compare -metric PSNR $1 .tmp.pbm /dev/null 2>&1)
-		cjb2 -dpi $(get_dpi ".$NAME.pbm") -lossy -losslevel $LOSSLVL .$NAME.pbm .$NAME.djvu
-		
-		echo "$1" $(wc -c < .$NAME.djvu) "bytes"
-		((N++))
-	fi
-;;
 	esac
 	shift
 done
+
 if [ -z "$COVER" ]
 then 
 	djvm -c "$OUT" .0*.djvu
